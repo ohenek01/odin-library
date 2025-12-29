016467f3d6e7fd33f8d95cc9a117cd445553e8df
ohenek01 <okosafo-affum@st.ug.edu.gh>
2025-12-29 14:04:05 +0000

    create Book constructor and addBook function

---
+++ script.js
@@ -0,0 +1,22 @@
+const myForm = document.querySelector("form");
+
+myForm.addEventListener("submit", () => {
+    event.preventDefault();
+
+
+})
+
+const myLibrary[];
+
+function Book(title, author, pages, read){
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read){
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
